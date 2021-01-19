2d47cfcbcc892836f1f5f03354b609707d2cfde8
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2021-01-19 13:55:52 -0500

    chore(client): bootstrapped next.js with chakra ui, eslint, prettier and
    husky in the project root
    
    Co-authored-by: Naasir Jusab <nasjuice7@gmail.com>

---
+++ client/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard",
+    "prettier",
+    "prettier/standard",
+    "prettier/react",
+    "prettier/prettier",
+    "prettier/babel",
+    "eslint:recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@babel", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/react-in-jsx-scope": "off",
+  },
+};
