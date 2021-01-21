3efede49aaff169a563a77df7c479695b4dde33d
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2021-01-21 14:16:33 -0500

    chore(client): example of making api call to server from client
    * added cors as dependency in server

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,10 +1,27 @@
 import { Flex, Heading } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 
-export const Hero = ({ title }) => (
-  <Flex justifyContent="center" alignItems="center" height="100vh">
-    <Heading fontSize="10vw">{title}</Heading>
-  </Flex>
-);
+export const Hero = ({ title }) => {
+  const [subtitle, setSubtitle] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+      fetch(baseUrl).then((data) => {
+        setSubtitle(`Request to backend :9090 status: ${data.status}`);
+        console.log(data);
+      });
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <Flex justifyContent="center" alignItems="center" height="100vh" flexDirection="column">
+      <Heading fontSize="10vw">{title}</Heading>
+      <Heading>{subtitle}</Heading>
+    </Flex>
+  );
+};
 
 Hero.defaultProps = {
   title: 'with-chakra-ui',
