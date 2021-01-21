3d9f081a026a0a5f3bde1f402ed39ab21ef1cf54
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2021-01-20 21:05:54 -0500

    chore: move makefile to root
    - add singleQuote config for prettier

---
+++ Makefile
@@ -0,0 +1,14 @@
+.DEFAULT_GOAL := help
+.PHONY: help run run-client run-server
+
+help: ## Show this help
+	@egrep -h '\s##\s' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[33m%-20s\033[0m %s\n", $$1, $$2}'
+
+run: ## Start up both client and server containers
+	docker-compose up --build
+
+run-client: ## Start up client container
+	docker-compose up --build client
+
+run-server: ## Start up server container
+	docker-compose up --build server
--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -4,30 +4,30 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "plugin:react/recommended",
-    "standard",
-    "prettier",
-    "prettier/standard",
-    "prettier/react",
-    "prettier/prettier",
-    "prettier/babel",
-    "eslint:recommended",
+    'plugin:react/recommended',
+    'standard',
+    'prettier',
+    'prettier/standard',
+    'prettier/react',
+    'prettier/prettier',
+    'prettier/babel',
+    'eslint:recommended',
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "@babel", "prettier"],
+  plugins: ['react', '@babel', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
-    "react/react-in-jsx-scope": "off",
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
 };
