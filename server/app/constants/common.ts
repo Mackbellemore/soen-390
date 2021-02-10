3818197ca61df7d40c702279e48b6a8bc48ba481
Naasir Jusab <nasjuice7@gmail.com>
2021-02-10 18:13:44 -0500

    Created new authorization middleware

--- server/app/constants/common.ts
+++ server/app/constants/common.ts
@@ -1,5 +1,3 @@
 const NON_AUTH_PATHS: string[] = ['/user/login', '/user/register'];
 
-const PATHS_FORBIDDEN_TO_GENERAL_ROLE: string[] = ['/user'];
-
-export { NON_AUTH_PATHS, PATHS_FORBIDDEN_TO_GENERAL_ROLE };
+export { NON_AUTH_PATHS };
--- server/app/controllers/UserController.ts
+++ server/app/controllers/UserController.ts
@@ -12,6 +12,7 @@ import {
 } from 'inversify-express-utils';
 import TYPES from '../constants/types';
 import config from 'config';
+import { checkAdminRole } from '../middlewares/authorization';
 
 @controller('/user')
 export class UserController extends BaseHttpController {
@@ -66,7 +67,7 @@ export class UserController extends BaseHttpController {
     return this.json(200);
   }
 
-  @httpGet('/')
+  @httpGet('/', checkAdminRole)
   public async get(): Promise<results.JsonResult> {
     try {
       const users: IUserEntity[] = await this.userService.getUsers();
