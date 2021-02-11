df34230d0238ba900f922b2df5862eb62a5a16e7
Mackenzie Bellemore <43047923+Mackbellemore@users.noreply.github.com>
2021-02-11 12:01:42 -0500

    Revert "feat(sprint 1): initial commit to main  (#17)" (#19)
    This reverts commit 9be0b8f3ac89d3db645a3d2e3cee01a2c07c258f.

--- server/app/constants/common.ts
+++
@@ -1,3 +0,0 @@
-const NON_AUTH_PATHS: string[] = ['/user/login', '/user/register'];
-
-export { NON_AUTH_PATHS };
--- server/app/errors/NotFoundError.ts
+++
@@ -1,6 +0,0 @@
-export default class NotFoundError extends Error {
-  constructor(message: string) {
-    super();
-    this.message = message;
-  }
-}
