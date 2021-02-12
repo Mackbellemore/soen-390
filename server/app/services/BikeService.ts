3fad0cdc663a43f53142e9489bce7dd6ad2c795c
Sujan Saravanamuthu <47992046+tbksujan@users.noreply.github.com>
2021-02-12 12:59:02 -0500

    feat(server): crud operations for bikes (#21)
    * update bike model
    
    * crud for bikes
    
    * testing
    
    * testing
    
    * eslint error
    
    Co-authored-by: Karin Kazarian <karinghazarian@gmail.com>

--- server/app/services/BikeService.ts
+++ server/app/services/BikeService.ts
@@ -2,6 +2,7 @@ import { IBike } from './../models/BikeModel';
 import { BikeRepository } from './../repository/BikeRepository';
 import { inject, injectable } from 'inversify';
 import TYPES from '../constants/types';
+import { NotFoundError } from '../errors';
 
 @injectable()
 export class BikeService {
@@ -13,5 +14,32 @@ export class BikeService {
 
   public async createBike(body: IBike): Promise<IBike> {
     return this.bikeRepo.create(body);
+  }
+
+  public async updateBike(id: string, body: IBike): Promise<IBike> {
+    const updatedBike = await this.bikeRepo.update(id, body);
+    if (!updatedBike) {
+      throw new NotFoundError(`Bike with id ${id} was not found`);
+    }
+
+    return updatedBike;
+  }
+
+  public async deleteBike(body: IBike): Promise<IBike | null> {
+    const deletedBike = await this.bikeRepo.delete(body.id);
+    if (!deletedBike) {
+      throw new NotFoundError(`Bike with id ${body.id} was not found`);
+    }
+
+    return deletedBike;
+  }
+
+  public async findById(id: string): Promise<IBike> {
+    const bike = await this.bikeRepo.findById(id);
+    if (!bike) {
+      throw new NotFoundError(`Material with name ${name} was not found`);
+    }
+
+    return bike;
   }
 }
