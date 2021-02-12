ea3f9659cdd641047468943d24b99764ecba3f33
Mackbellemore <m.bellemore@hotmail.com>
2021-02-12 17:29:04 -0500

    chore(deployment): fix configs for mongo

--- server/config/staging.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongo: {
-    host: process.env.STAGING_MONGO_HOST,
-    user: process.env.STAGING_MONGO_USER,
-    pass: process.env.STAGING_MONGO_PASS,
-    db: process.env.STAGING_MONGO_DB_NAME,
-  },
-};
