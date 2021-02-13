3f6d708f6535704256b8f466ca88757854b0d854
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2021-02-12 22:15:01 -0500

    fix: attempt cors issue

--- server/app/App.ts
+++ server/app/App.ts
@@ -35,7 +35,9 @@ export class App {
 
       appBuilder.setConfig((server: Application) => {
         // middlewares
-        server.use(cors({ credentials: true, origin: this.config.get<string>('origin') }));
+        server.use(
+          cors({ credentials: true, origin: ['http://localhost:3000', /\.netlify\.app$/] })
+        );
         server.use(cookieParser());
         server.use(
           bodyParser.urlencoded({
