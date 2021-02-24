48534beb6ee971840732144c10cb9fba44e24ec9
Karin Kazarian <karinghazarian@gmail.com>
2021-02-24 17:25:57 -0500

    add heading

--- client/src/components/views/Inventory.jsx
+++ client/src/components/views/Inventory.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
+import { Tab, TabList, TabPanel, TabPanels, Tabs, Heading } from '@chakra-ui/react';
 import Head from 'next/head';
 import Materials from './inventory items/Materials';
 import Parts from './inventory items/Parts';
@@ -11,6 +11,9 @@ const Inventory = () => {
       <Head>
         <title>ERP - Inventory</title>
       </Head>
+      <Heading size="xl" textAlign="center" mt={5}>
+        Inventory
+      </Heading>
       <Tabs>
         <TabList>
           <Tab>Materials</Tab>
