f8700591da77cf1285aa053700ac8eb6cc6c209a
Mackbellemore <m.bellemore@hotmail.com>
2021-03-03 18:07:08 -0500

    feat(server): make type immutable

--- server/app/models/PartModel.ts
+++ server/app/models/PartModel.ts
@@ -17,7 +17,7 @@ export const PartSchema: Schema = new Schema({
   name: { type: Schema.Types.String, required: true, unique: true, immutable: true },
   quality: { type: Schema.Types.String, required: true },
   description: { type: Schema.Types.String, required: false },
-  type: { type: Schema.Types.String, required: true },
+  type: { type: Schema.Types.String, required: true, immutable: true },
   color: { type: Schema.Types.String, required: false },
   finish: { type: Schema.Types.String, required: false },
   grade: { type: Schema.Types.String, required: false },
