3bccb621daf7ffee6fcb4b8bc6ce28ea6840b0fe
Mackenzie Bellemore <43047923+Mackbellemore@users.noreply.github.com>
2021-03-04 18:12:38 -0500

    chore(server): add inventory management for parts and materials (#57)
    * feat(server): add stock / inventory updates for Parts
    
    * feat(server): fix stock validation
    
    * feat(server): make type immutable

--- server/app/models/PartModel.ts
+++ server/app/models/PartModel.ts
@@ -1,23 +1,26 @@
+import { partType } from '../entities/Part';
 import { Document, Schema } from 'mongoose';
 
 export interface IPart extends Document {
   name: string;
   quality: string;
   description: string;
-  type: string;
+  type: partType;
   color: string;
   finish: string;
   grade: string;
   detail: string;
+  stock: number;
 }
 
 export const PartSchema: Schema = new Schema({
   name: { type: Schema.Types.String, required: true, unique: true, immutable: true },
   quality: { type: Schema.Types.String, required: true },
   description: { type: Schema.Types.String, required: false },
-  type: { type: Schema.Types.String, required: true },
+  type: { type: Schema.Types.String, required: true, immutable: true },
   color: { type: Schema.Types.String, required: false },
   finish: { type: Schema.Types.String, required: false },
   grade: { type: Schema.Types.String, required: false },
   detail: { type: Schema.Types.String, required: false },
+  stock: { type: Schema.Types.Number, required: true },
 });
--- server/app/services/PartService.ts
+++ server/app/services/PartService.ts
@@ -1,12 +1,19 @@
+import { MaterialService } from './MaterialService';
 import { IPart } from '../models/PartModel';
 import { PartRepository } from './../repository/PartRepository';
 import TYPES from '../constants/types';
 import { injectable, inject } from 'inversify';
-import { NotFoundError } from '../errors';
+import { BadRequestError, NotFoundError } from '../errors';
+import { IMaterial } from '../models/MaterialModel';
+import { partTypeMaterials } from '../validation/parts/partTypes';
+import { partType } from './../entities/Part';
 
 @injectable()
 export class PartService {
-  constructor(@inject(TYPES.PartRepository) private partRepo: PartRepository) {}
+  constructor(
+    @inject(TYPES.MaterialService) private materialService: MaterialService,
+    @inject(TYPES.PartRepository) private partRepo: PartRepository
+  ) {}
 
   public async get(id?: string, name?: string): Promise<IPart[] | IPart> {
     if (!id && !name) {
@@ -35,18 +42,56 @@ export class PartService {
   }
 
   public async createPart(body: IPart): Promise<IPart> {
-    return this.partRepo.create(body);
+    await this.handleMaterialStockUpdates(body.stock, body.type);
+    const part = await this.partRepo.create(body);
+
+    return part;
   }
 
   public async updatePart(name: string, body: IPart): Promise<IPart | null> {
-    const updatedPart = await this.partRepo.updateByName(name, body);
-    if (!updatedPart) {
-      throw new NotFoundError(`Part with name ${name} was not found`);
+    const originalPart = (await (this.get(undefined, name) as unknown)) as IPart;
+
+    // if were increasing the stock, then make sure we have the materials for it
+    if (body.stock > originalPart.stock) {
+      await this.handleMaterialStockUpdates(body.stock - originalPart.stock, originalPart.type);
     }
-    return updatedPart;
+
+    return this.partRepo.updateByName(name, body);
   }
 
   public async deletePart(name: string): Promise<IPart | null> {
     return this.partRepo.deletePart(name);
+  }
+
+  // function to validate and update material stocks when creating a new part
+  public async handleMaterialStockUpdates(partStock: number, partType: partType): Promise<void> {
+    // list of material and stock needed defined under app/validation/parts/partTypes.ts
+    const materialList = partTypeMaterials[partType];
+
+    // loop over all the materials to check stock availability and update stocks accordingly
+    const callStack = [];
+    for (const [materialName, materialStock] of Object.entries(materialList)) {
+      // multiply the material stock by the amount of parts we want to add
+      const neededStock = Number(materialStock) * partStock;
+
+      const { stock } = await this.materialService.findMaterial(materialName);
+
+      // check if we have the amount of stock needed for each required material to create the part
+      if (stock < Number(neededStock)) {
+        throw new BadRequestError(
+          `Unable to create this part, required ${neededStock} ${materialName} but only found ${stock} in stock`
+        );
+      }
+
+      // if we have the stock needed then store the call to decrement the material stock
+      callStack.push(() =>
+        this.materialService.updateMaterial(materialName, {
+          stock: stock - Number(neededStock),
+        } as IMaterial)
+      );
+    }
+
+    // Call all the updates stored in the callStack
+    await Promise.all(callStack.map((update) => update()));
   }
 }
