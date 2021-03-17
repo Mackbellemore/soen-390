26a7eca2263ce11c6b543fc7162f54d6ee821ba2
Mackenzie Bellemore <43047923+Mackbellemore@users.noreply.github.com>
2021-03-17 12:27:43 -0400

    chore(server): link parts to bikes (#63)

--- .docker/docker-entrypoint-initdb.d/1-init.js
+++ .docker/docker-entrypoint-initdb.d/1-init.js
@@ -10,6 +10,9 @@ db.createUser({
 });
 
 db = new Mongo().getDB("app_db");
+db.materials.createIndex({ name: 1 }, { unique: true });
+db.parts.createIndex({ name: 1 }, { unique: true });
+db.email.createIndex({ email: 1 }, { unique: true });
 
 db.bikes.insert([
   {
@@ -38,43 +41,102 @@ db.materials.insert([
   {
     description: "steel",
     name: "steel",
-    stock: 100,
+    stock: 1000,
     weight: "kg",
     price: 5,
   },
   {
     description: "rubber",
     name: "rubber",
-    stock: 100,
+    stock: 1000,
     weight: "kg",
     price: 77,
   },
   {
     description: "leather",
     name: "leather",
-    stock: 100,
+    stock: 1000,
     weight: "kg",
     price: 44,
   },
   {
     description: "copper",
     name: "copper",
-    stock: 100,
+    stock: 1000,
     weight: "kg",
     price: 3,
   },
   {
     description: "plastic",
     name: "plastic",
-    stock: 100,
+    stock: 1000,
     weight: "kg",
     price: 1,
   },
   {
     description: "aluminum",
     name: "aluminum",
-    stock: 100,
+    stock: 1000,
     weight: "kg",
     price: 10,
+  },
+]);
+
+db.parts.insert([
+  {
+    name: "handle-bar3000",
+    quality: "aluminium",
+    type: "handle_bar",
+    color: "red",
+    stock: 1000,
+  },
+  {
+    name: "fast-wheels",
+    quality: "aluminium",
+    type: "wheels",
+    color: "red",
+    stock: 1000,
+  },
+  {
+    name: "drip",
+    quality: "aluminium",
+    type: "chain",
+    color: "red",
+    stock: 1000,
+  },
+  {
+    name: "nice-frame",
+    quality: "aluminium",
+    type: "frame",
+    color: "red",
+    stock: 1000,
+  },
+  {
+    name: "pedal-4u",
+    quality: "aluminium",
+    type: "pedal",
+    color: "red",
+    stock: 1000,
+  },
+  {
+    name: "breaks",
+    quality: "aluminium",
+    type: "brakes",
+    color: "red",
+    stock: 1000,
+  },
+  {
+    name: "seet",
+    quality: "aluminium",
+    type: "seat",
+    color: "red",
+    stock: 1000,
+  },
+  {
+    name: "knife",
+    quality: "aluminium",
+    type: "fork",
+    color: "red",
+    stock: 1000,
   },
 ]);
