294c17340754c5d7c3299e1da5abba0ee358366a
Karin Kazarian <karinghazarian@gmail.com>
2021-03-27 18:53:27 -0400

    add enum to defect model

--- server/app/models/DefectModel.ts
+++ server/app/models/DefectModel.ts
@@ -11,7 +11,12 @@ export interface IDefect extends Document {
 export const DefectSchema: Schema = new Schema({
   id: { type: Schema.Types.Number, required: true, unique: true, immutable: true },
   type: { type: Schema.Types.String, required: true },
-  status: { type: Schema.Types.String, required: true },
+  status: {
+    type: Schema.Types.String,
+    required: true,
+    enum: ['Solved', 'Pending', 'Ongoing'],
+    default: 'Pending',
+  },
   description: { type: Schema.Types.String, required: false },
   partName: { type: Schema.Types.String, required: true },
 });
