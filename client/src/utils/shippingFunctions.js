614bf86b7471e709a6c98183d702a985972f80aa
DavidLiang01 <60051944+DavidLiang01@users.noreply.github.com>
2021-03-27 17:38:08 -0400

    refactor(client): removed unused function

--- client/src/utils/shippingFunctions.js
+++ client/src/utils/shippingFunctions.js
@@ -7,16 +7,10 @@ const getDestinations = (shippings) => {
   });
 };
 
-const getShippingIds = (shippings) => {
-  return shippings.map((shpmt) => {
-    return shpmt._id;
-  });
-};
-
 const getShipment = (shippings, _id) => {
   return shippings.find((shpmt) => {
     return String(shpmt._id) === String(_id);
   });
 };
 
-export { getDestinations, getShippingIds, getShipment };
+export { getDestinations, getShipment };
