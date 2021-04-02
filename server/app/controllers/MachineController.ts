df2055174dd7e1fbf58dc26b60a9b4c96e37e810
Naasir Jusab <nasjuice7@gmail.com>
2021-04-02 14:24:00 -0400

    Machine controller comments

--- server/app/controllers/MachineController.ts
+++ server/app/controllers/MachineController.ts
@@ -5,6 +5,7 @@ import TYPES from '../constants/types';
 import { BaseController } from './BaseController';
 import { IMachine } from '../models/MachineModel';
 import { MachineService } from './../services/MachineService';
+import { Doc } from 'inversify-express-doc';
 
 @controller('/machine')
 export class MachineController extends BaseController {
@@ -12,6 +13,13 @@ export class MachineController extends BaseController {
     super();
   }
 
+  @Doc('Get All Machines')
+  /**
+   * @desc          Get All Machines
+   * @route         GET /machine
+   * @access        Public
+   * @returns       List Machine JSON Format
+   */
   @httpGet('/')
   public async get(): Promise<results.JsonResult> {
     try {
@@ -22,6 +30,14 @@ export class MachineController extends BaseController {
     }
   }
 
+  @Doc('Create Machine')
+  /**
+   * @desc          Create Machine
+   * @route         POST /machine
+   * @access        Public
+   * @param request
+   * @returns       Machine JSON Format
+   */
   @httpPost('/')
   public async post(request: Request): Promise<results.JsonResult> {
     try {
@@ -32,6 +48,14 @@ export class MachineController extends BaseController {
     }
   }
 
+  @Doc('Delete Scheduling')
+  /**
+   * @desc          Delete Machine
+   * @route         DELETE /machine
+   * @access        Public
+   * @param request
+   * @returns       Machine JSON Format
+   */
   @httpDelete('/')
   public async delete(request: Request): Promise<results.JsonResult> {
     try {
@@ -42,6 +66,14 @@ export class MachineController extends BaseController {
     }
   }
 
+  @Doc('Get Machine by Name')
+  /**
+   * @desc          Get Machine By Name
+   * @route         GET /machine/:machineName
+   * @access        Public
+   * @param request
+   * @returns       Machine JSON Format
+   */
   @httpGet('/:machineName')
   public async getByMachineName(request: Request): Promise<results.JsonResult> {
     try {
