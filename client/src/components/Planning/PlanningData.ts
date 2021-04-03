3b3dc49a2174bf0ea190bc95d75e2c73cec091ae
Tiffany Zeng <tiffany.zeng@hotmail.com>
2021-04-03 19:06:22 -0400

    feat: added bar graphs to planning page

---
+++ client/src/components/Planning/PlanningData.ts
@@ -0,0 +1,103 @@
+//Mock data for Planning Graphs
+
+export const productionData = [
+  {
+    year: 2013,
+    roadBike: 300,
+    mountainBike: 200,
+    others: 250,
+  },
+  {
+    year: 2014,
+    roadBike: 500,
+    mountainBike: 600,
+    others: 750,
+  },
+  {
+    year: 2015,
+    roadBike: 800,
+    mountainBike: 700,
+    others: 950,
+  },
+  {
+    year: 2016,
+    roadBike: 1000,
+    mountainBike: 900,
+    others: 1200,
+  },
+  {
+    year: 2017,
+    roadBike: 1500,
+    mountainBike: 1700,
+    others: 2000,
+  },
+  {
+    year: 2018,
+    roadBike: 2500,
+    mountainBike: 2750,
+    others: 3000,
+  },
+  {
+    year: 2019,
+    roadBike: 3000,
+    mountainBike: 2900,
+    others: 3500,
+  },
+  {
+    year: 2020,
+    roadBike: 4000,
+    mountainBike: 3200,
+    others: 3700,
+  },
+];
+
+export const plData = [
+  {
+    year: 2013,
+    revenue: 4000,
+    cost: 3500,
+    profit: 500,
+  },
+  {
+    year: 2014,
+    revenue: 6000,
+    cost: 4500,
+    profit: 1500,
+  },
+  {
+    year: 2015,
+    revenue: 7000,
+    cost: 6000,
+    profit: 1000,
+  },
+  {
+    year: 2016,
+    revenue: 8000,
+    cost: 7800,
+    profit: 200,
+  },
+  {
+    year: 2017,
+    revenue: 9000,
+    cost: 8300,
+    profit: 700,
+  },
+  {
+    year: 2018,
+    revenue: 10000,
+    cost: 9500,
+    profit: 500,
+  },
+  {
+    year: 2019,
+    revenue: 11000,
+    cost: 9900,
+    profit: 1100,
+  },
+  {
+    year: 2020,
+    revenue: 12000,
+    cost: 10750,
+    profit: 1250,
+  },
+];
--- client/src/components/views/Planning.jsx
+++ client/src/components/views/Planning.jsx
@@ -1,5 +1,18 @@
-import { Heading } from '@chakra-ui/react';
+import { Heading, SimpleGrid, Text, Center, Box } from '@chakra-ui/react';
+import useWindowSize from 'hooks/useWindowSize';
+import { productionData, plData } from 'components/Planning/PlanningData.ts';
 import Head from 'next/head';
+import React from 'react';
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Tooltip,
+  XAxis,
+  YAxis,
+  // eslint-disable-next-line import/no-unresolved
+} from 'recharts';
 
 const PlanningHeader = () => (
   <>
@@ -13,9 +26,59 @@ const PlanningHeader = () => (
 );
 
 const Planning = () => {
+  const windowSize = useWindowSize();
+
   return (
     <>
       <PlanningHeader />
+      <SimpleGrid columns={[1, null, 2]} spacing={5}>
+        <Box>
+          <Text fontSize="2xl" align="center">
+            Expected Production
+          </Text>
+          {productionData.length > 0 && (
+            <Center border="1px" borderColor="gray.200" ml={5} boxShadow="xl" borderTopRadius="xl">
+              <BarChart
+                width={windowSize.width / 2.5}
+                height={windowSize.height / 2.5}
+                data={productionData}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="year" />
+                <YAxis />
+                <Tooltip />
+                <Legend verticalAlign="top" />
+                <Bar name="Road Bike" dataKey="roadBike" fill="#5be7c4" />
+                <Bar name="Mountain Bike" dataKey="mountainBike" fill="#4fc1e9" />
+                <Bar name="Others" dataKey="others" fill="#7a57d1" />
+              </BarChart>
+            </Center>
+          )}
+        </Box>
+        <Box>
+          <Text fontSize="2xl" align="center">
+            Expected Profit & Loss
+          </Text>
+          {plData.length > 0 && (
+            <Center border="1px" borderColor="gray.200" ml={5} boxShadow="xl" borderTopRadius="xl">
+              <BarChart
+                width={windowSize.width / 2.5}
+                height={windowSize.height / 2.5}
+                data={plData}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="year" />
+                <YAxis />
+                <Tooltip />
+                <Legend verticalAlign="top" />
+                <Bar name="Revenue" dataKey="revenue" fill="#ff008e" />
+                <Bar name="Cost" dataKey="cost" fill="#124e96" />
+                <Bar name="Profit" dataKey="profit" fill="#0d8abc" />
+              </BarChart>
+            </Center>
+          )}
+        </Box>
+      </SimpleGrid>
     </>
   );
 };
